1d6911e0abcbd285cd71abfedd7e76938ff449c8
Natalie <sonatalex@gmail.com>
2024-07-07 16:11:08 +0300

    first example

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// принимает на вход трехзначное число и убирает вторую цифру
+
+
+
+/* System.Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number > 99 && number < 1000)
+{
+    
+    int result = number / 100 * 10 + number % 10;
+    System.Console.WriteLine(result);
+
+}
+else
+{
+    System.Console.WriteLine("Вы ввеели не трехначное число");
+}
+*/
